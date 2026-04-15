ca644726dbe1b4d754e977b4b3033474fad7d20e
Opilane TTHK <opilane@tthk.ee>
2026-04-15 13:25:07 +0300

    Update App.tsx

--- VeebTARge25/04SPAreactToDoList/ToDoList/src/App.tsx
+++ VeebTARge25/04SPAreactToDoList/ToDoList/src/App.tsx
@@ -11,8 +11,20 @@ type ToDo = {
 const App: React.FC = () => {
   const [toDos, setToDos] = useState <ToDo[]>([]);
   const [newToDo, setNewToDo] = useState('');
+//useState on reacti hook, mis võimaldab meil lisada komponentidele olekut
+//see tagastab massiivi, kus esimene element on praegune olek ja teine
+//Element on funktsioon, mida saab kasutada oleku värskendamiseks
 
-
+const addToDo = () => {
+  if (!newToDo.trim()) return; //ignore empty tasks
+  setToDos([...toDos, { id: Date.now(), text: newToDo.trim(), completed: false}]);
+  // Date.now() anna meile unikaalse ID, mis põhineb pragusest ajast
+  //ToDos tähendab, et me võtame olemasolevad ülesanded ja lisame uue ülesande massiivi lõppu
+  //newToDo.trim eemdaldab tühikud testi algusest ja lõpust, et vältida tühjade ülesannete lisamist
+  // completed: false tähendab, et uus ülesanne on algselt lõpetamata.
+  setNewToDo('');
+  //pärast uue ülesande lisamist tühjendame sisendvälja, et kasutaja saaks kohe uue ülesande lisada
+};
 
 //function App() {
 
@@ -30,6 +42,16 @@ const App: React.FC = () => {
           />
           <button onClick={addToDo}>Add</button>
       </div>
+      <ul className="todo-list">
+        {toDos.map(toDo => (
+          <ToDoItem
+            key={toDo.id}
+            toDo={toDo}
+              toggleToDo={toggleToDo}
+              deleteToDo={deleteToDo}
+              />       
+        ))}
+      </ul>
     </div>
   )
 //}
