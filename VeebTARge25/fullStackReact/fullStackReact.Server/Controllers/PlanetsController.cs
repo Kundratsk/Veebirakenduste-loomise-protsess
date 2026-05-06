452c493c00e35e4886345927314984161e5a1047
Opilane TTHK <opilane@tthk.ee>
2026-05-06 13:12:49 +0300

    Fullstack

---
+++ VeebTARge25/fullStackReact/fullStackReact.Server/Controllers/PlanetsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fullStackReact.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlanetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
